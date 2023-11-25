d7c264f60522b55ff03ded3b87630bd36c54fe14
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-11-25 22:21:17 +0100

    done login modal design

--- src/components/Pages/LoginModal/LoginModal.jsx
+++ src/components/Pages/LoginModal/LoginModal.jsx
@@ -1,16 +1,73 @@
 import React from "react";
 import { RxCross2 } from "react-icons/rx";
+import logo from "../../../assets/login_logo.png";
+import facebook from "../../../assets/facebook.png";
+import google from "../../../assets/google.png";
 
 const LoginModal = ({ visible, onClose }) => {
   if (!visible) return null;
 
   return (
     <div className=" fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center">
-      <div className="bg-white w-[540px] h-[480px] rounded-sm">
-        <div className="top-0 right-0 absolute w-10 h-10 rounded-full bg-white shadow-md grid place-items-center">
-          <RxCross2 onClick={onClose} color="#C71E66" size={25} />
+      <div className="bg-white w-[540px] h-[480px] rounded-sm relative">
+        <div className="absolute top-0 right-0 -mt-3 -mr-3">
+          <div className="w-10 h-10 rounded-full bg-white shadow-md grid place-items-center">
+            <RxCross2 onClick={onClose} color="#C71E66" size={25} />
+          </div>
         </div>
-        <p>modal</p>
+        <div className="flex flex-col justify-center items-center font-poppins h-full">
+          <div className="mb-6">
+            <img className="w-[125px] h-[70px]" src={logo} alt="Company Logo" />
+          </div>
+
+          <div className="mb-4">
+            <p>Login with</p>
+          </div>
+
+          <div className="w-[300px] my-6">
+            <a
+              className="bg-[#C71E66] hover:bg-[#c35b88] text-white rounded-md text-md px-[89px] py-3"
+              href="#"
+            >
+              Mobile Number
+            </a>
+          </div>
+
+          <div className="flex justify-between w-[300px] mb-6">
+            <div>
+              <a
+                className="flex justify-center items-center gap-3 bg-white hover:bg-[#FAFBFC] text-gray-400 px-8 py-2 rounded-md border border-[#bfc8d1]"
+                href="#"
+              >
+                <img className="w-4 h-4" src={facebook} alt="Facebook Logo" />
+                <span className=" text-sm">Facebook</span>
+              </a>
+            </div>
+
+            <div className="ml-3">
+              <a
+                className="flex justify-center items-center gap-3 bg-white hover:bg-[#FAFBFC] text-gray-400 px-8 py-2 rounded-md border border-[#bfc8d1]"
+                href="#"
+              >
+                <img className="w-4 h-4" src={google} alt="Google Logo" />
+                <span className=" text-sm">Google</span>
+              </a>
+            </div>
+          </div>
+
+          <div className="flex mt-3 text-sm">
+            <div>Don’t have an account in Sheba.xyz?</div>
+
+            <div className="ml-3">
+              <a
+                className="text-[#C71E66] hover:text-white gap-3 bg-white hover:bg-[#C71E66] px-5 py-2 rounded-md text-xs font-semibold border border-[#C71E66]"
+                href="#"
+              >
+                Signup Now
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCNqyWFwXTu8spD0q4zBskeU2jEn9DMYk",
+  authDomain: "sheba-d878b.firebaseapp.com",
+  projectId: "sheba-d878b",
+  storageBucket: "sheba-d878b.appspot.com",
+  messagingSenderId: "345850405544",
+  appId: "1:345850405544:web:2d7c24365c129cbf37589d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
